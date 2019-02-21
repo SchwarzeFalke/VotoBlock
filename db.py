d145e890ece6ac695476b11cf586562a2adda9e1
Carlos Vara <carlos.vara@alumnos.udg.mx>
2019-02-21 01:03:55 -0600

    Adding .env and setting env variables, adding requirements and creating db connection

---
+++ db.py
@@ -0,0 +1,17 @@
+# @Author: schwarze_falke
+# @Date:   2019-02-21T00:51:54-06:00
+# @Last modified by:   schwarze_falke
+# @Last modified time: 2019-02-21T01:02:41-06:00
+import MySQLdb
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = MySQLdb.connect(host=os.getenv("DB_HOST"), user=os.getenv("DB_USER"),
+                     passwd=os.getenv("DB_PASS"), db=os.getenv("DB_NAME"))
+cursor = db.cursor()
+cursor.execute("SHOW TABLES")
+for row in cursor.fetchall():
+    print(row[0])
+db.close()
