130f56285e4cc1c642971d219954d5e8cc85e3e9
Carlos Vara <carlos.vara@alumnos.udg.mx>
2019-05-20 15:27:40 -0500

    Adding database env variable

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()  # Load .env file with the env variables
 
 connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
                                      host=os.getenv("DB_HOST"),
-                                     database=os.getenv("DB"))
+                                     database=os.getenv("DB_NAME"))
 cursor = connection.cursor()
 
 app = Flask(__name__)
