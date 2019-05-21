bd145f3b36c76299cd5f7fc4ba06f9b7c67018ab
Carlos Vara <carlos.vara@alumnos.udg.mx>
2019-05-21 03:34:25 -0500

    Fixing database connection (I hope)

--- app.py
+++ app.py
@@ -12,24 +12,28 @@ import json
 
 load_dotenv()  # Load .env file with the env variables
 
-connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
-                                     host=os.getenv("DB_HOST"),
-                                     database=os.getenv("DB_NAME"))
-cursor = connection.cursor()
-
 app = Flask(__name__)
 cors = CORS(app)
 
 
 @app.route('/')
 def index():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     response = make_response(str('Welcome to VotoBlock!'))
     response.headers.add('Access-Control-Allow-Origin', '*')
+    connection.close()
     return response
 
 
 @app.route('/verify/elections/', methods=['GET'])
 def verifyElectionPeriod():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     db = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
                                  host=os.getenv("DB_HOST"),
                                  database=os.getenv("DB_NAME"))
@@ -37,11 +41,16 @@ def verifyElectionPeriod():
     cursor = db.cursor()
     cursor.execute("SELECT _id, descript FROM election")
     elections = cursor.fetchall()
+    connection.close()
     return jsonify(elections)
 
 
 @app.route('/election/', methods=['GET'])
 def getElectionResults():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     db = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
                                  host=os.getenv("DB_HOST"),
                                  database=os.getenv("DB_NAME"))
@@ -52,11 +61,16 @@ def getElectionResults():
     elections = cursor.fetchall()
     electionDictionary = {}
     lista = []
+    connection.close()
     return jsonify(elections)
 
 
 @app.route('/verify/candidates/', methods=['GET'])
 def verifyAvailableCandidates():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     db = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
                                  host=os.getenv("DB_HOST"),
                                  database=os.getenv("DB_NAME"))
@@ -66,6 +80,7 @@ def verifyAvailableCandidates():
     cursor.execute(
         "SELECT electoral_key, name, middle_name, mlastname, flastname FROM candidate WHERE election_id = '{}'".format(electionId))
     candidates = cursor.fetchall()
+    connection.close()
     return jsonify(candidates)
 
 
@@ -81,11 +96,16 @@ def getCandidateInformation():
     cursor.execute(
         "SELECT * FROM candidate WHERE electoral_key = '{}'".format(electoral_key))
     candidate = cursor.fetchall()
+    connection.close()
     return jsonify(candidate)
 
 
 @app.route('/login/', methods=['POST'])
 def login():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     login_access = Access(connection, cursor)
     electoral_key = request.form.get('electoral_key')
     password = request.form.get('pass')
@@ -95,11 +115,16 @@ def login():
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    connection.close()
     return response
 
 
 @app.route('/signup/', methods=['POST'])
 def signup():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     signup_access = Access(connection, cursor)
     electoral_key = request.form.get('electoral_key')
     password = request.form.get('pass')
@@ -108,22 +133,32 @@ def signup():
         str(signup_access.register(str(electoral_key), str(password), str(mail)))
     )
     response.headers.add('Access-Control-Allow-Origin', '*')
+    connection.close()
     return response
 
 # Section: Vote routes
 @app.route('/vote', methods=['GET'])
 def getVote():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     vote = Vote(connection, cursor)
     voteKey = request.form.get('vote')
     response = make_response(
         str(vote.retrieveVote(voteKey))[2:-1]
     )
     response.headers.add('Access-Control-Allow-Origin', '*')
+    connection.close()
     return response
 
 
 @app.route('/vote', methods=['POST'])
 def postVote():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     vote = Vote(connection, cursor)
     voterKey = request.form.get('voter')
     electionKey = request.form.get('election')
@@ -134,39 +169,55 @@ def postVote():
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    connection.close()
     return response
 
 
 @app.route('/fake/user', methods=['GET'])
 def fake_user():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     faking = Fakerism(connection, cursor)
     response_user = str(faking.fake_user())
     response = make_response(response_user, 200)
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    connection.close()
     return response
 
 
 @app.route('/fake/voter', methods=['GET'])
 def fake_voter():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     faking = Fakerism(connection, cursor)
     response_voter = str(faking.fake_voter())
     response = make_response(response_voter, 200)
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    connection.close()
     return response
 
 
 @app.route('/fake/candidate', methods=['GET'])
 def fake_candidate():
+    connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"),
+                                         host=os.getenv("DB_HOST"),
+                                         database=os.getenv("DB_NAME"))
+    cursor = connection.cursor()
     faking = Fakerism(connection, cursor)
     response_candidate = str(faking.fake_candidate())
     response = make_response(response_candidate, 200)
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    connection.close()
     return response
 
 
