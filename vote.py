f6f126ab44da3d5cb528f30b9e5742eac874a9e7
Carlos Vara <carlos.vara@alumnos.udg.mx>
2019-05-21 09:51:54 -0500

    fix this shit

--- vote.py
+++ vote.py
@@ -11,18 +11,18 @@ class Vote:
     def generateVote(self, electionKey, voterKey, candidateKey):
         composedKey = electionKey+voterKey+candidateKey
         hashKey = hashlib.sha256(composedKey.encode('utf-8')).hexdigest()[0:32]
-        print(composedKey.rjust(28))
+        print(composedKey.rjust(32))
         cypher = AES.new(hashKey.rjust(32), AES.MODE_ECB)
         encoded = base64.b64encode(cypher.encrypt(
-            composedKey.rjust(28)))
-        i = 32
-        pointer = -1
+            composedKey[0:32])
+        i=32
+        pointer=-1
         while(i > 0):
             self.generateDataBlock(hashKey[i-8:i], pointer)
-            pointer = self.searchDataBlock(hashKey[i-8:i])
+            pointer=self.searchDataBlock(hashKey[i-8:i])
             i -= 8
-        finalPointer = self.searchDataBlock(hashKey[0:8])
-        insert = (
+        finalPointer=self.searchDataBlock(hashKey[0:8])
+        insert=(
             "INSERT INTO vote(_data_block_pointer, _encrypted_info) VALUES(%s, %s)"
         )
         self.cursor.execute(insert, (finalPointer, encoded,))
@@ -30,34 +30,34 @@ class Vote:
         return 'OK'
 
     def generateDataBlock(self, block, pointer):
-        insert = ("INSERT INTO data_block(_hash, _pointer) VALUES(%s, %s)")
+        insert=("INSERT INTO data_block(_hash, _pointer) VALUES(%s, %s)")
         self.cursor.execute(insert, (block, pointer,))
         self.connection.commit()
 
     def retrieveDataBlock(self, pointer):
-        search = ("SELECT _hash, _pointer FROM data_block WHERE _id = %s")
+        search=("SELECT _hash, _pointer FROM data_block WHERE _id = %s")
         self.cursor.execute(search, (pointer,))
         return self.cursor.fetchall()
 
     def searchDataBlock(self, block):
-        search = ("SELECT _id FROM data_block WHERE _hash = %s")
+        search=("SELECT _id FROM data_block WHERE _hash = %s")
         self.cursor.execute(search, (block,))
         return self.cursor.fetchone()[0]
 
     def retrieveVote(self, key):
-        search = (
+        search=(
             "SELECT _data_block_pointer, _encrypted_info FROM vote WHERE _id = %s"
         )
         self.cursor.execute(search, (key,))
-        tempData = self.cursor.fetchall()
-        pointer = tempData[0][0]
-        encoded = tempData[0][1]
-        secretKey = ""
+        tempData=self.cursor.fetchall()
+        pointer=tempData[0][0]
+        encoded=tempData[0][1]
+        secretKey=""
         while(pointer != -1):
-            tempData = self.retrieveDataBlock(pointer)
+            tempData=self.retrieveDataBlock(pointer)
             secretKey += tempData[0][0]
-            pointer = tempData[0][1]
+            pointer=tempData[0][1]
 
-        cipher = AES.new(secretKey, AES.MODE_ECB)
-        decoded = cipher.decrypt(base64.b64decode(encoded))
+        cipher=AES.new(secretKey, AES.MODE_ECB)
+        decoded=cipher.decrypt(base64.b64decode(encoded))
         return decoded.strip()
